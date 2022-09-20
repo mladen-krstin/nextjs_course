194746162ce5d85f2a68ccd6436638aa1fa4954b
Mladen Krstin <mladen.krstin@gmail.com>
2022-09-20 19:26:36 +0200

    20.09.2022.

--- nextjs-course-api-routes/pages/api/comments/[eventId].js
+++ nextjs-course-api-routes/pages/api/comments/[eventId].js
@@ -10,11 +10,11 @@ async function handler(req, res) {
     const { email, name, text } = req.body;
 
     if (
-      !email.includes('@') ||
+      !email.includes("@") ||
       !name ||
-      name.trim() === '' ||
+      name.trim() === "" ||
       !text ||
-      text.trim() === ''
+      text.trim() === ""
     ) {
       res.status(422).json({ message: 'Invalid input.' });
       return;
@@ -26,6 +26,8 @@ async function handler(req, res) {
       eventId
     };
 
+    console.log("new comment", newComment);
+
     const dbName = 'events';
 
     async function main() {
@@ -35,24 +37,32 @@ async function handler(req, res) {
       const result = await db.collection('comments').insertOne({ newComment });
 
       console.log(result);
+      newComment.id = result.insertedId;
 
       res.status(201).json({ message: 'Added comment.', comment: newComment });
     }
+    main()
+      .then(console.log('Test'))
+      .catch(console.error)
+      .finally(() => client.close());
   }
 
   if (req.method === 'GET') {
-    const dummyList = [
-      { id: 'c1', name: 'Max', text: 'A first comment!' },
-      { id: 'c2', name: 'Manuel', text: 'A second comment!' },
-    ];
+    const dbName = 'events';
 
-    res.status(200).json({ comments: dummyList });
+    await client.connect();
+    const db = client.db(dbName);
+
+    const documents = await db
+      .collection('comments')
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
+
+
+    res.status(200).json({ comments: documents });
   }
-
-  // main()
-  //   .then(console.log(userEmail))
-  //   .catch(console.error)
-  //   .finally(() => client.close());
+  client.close();
 }
 
 
