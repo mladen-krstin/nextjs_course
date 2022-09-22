cec582de34339c3a49bbfd4e763b5da02c44ddbe
Mladen Krstin <mladen.krstin@gmail.com>
2022-09-22 18:56:25 +0200

    22.09.2022.

--- nextjs-course-api-routes/helpers/db-utils.js
+++ nextjs-course-api-routes/helpers/db-utils.js
@@ -1,12 +1,9 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 export async function connectDatabase() {
   const url = "mongodb+srv://korisnik:kupDmeGAR8J2FrFv@cluster0.2uwmvmr.mongodb.net/?retryWrites=true&w=majority";
-  // const client = new MongoClient(url);
-  // await client.connect();
-  // console.log('Connected successfully to server');
 
-  const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  const client = new MongoClient(url);
 
   return client;
 }
@@ -15,12 +12,6 @@ export async function insertDocument(client, collection, document) {
   const dbName = 'events';
   const result = await db.collection(collection).insertOne(document);
   const db = client.db(dbName);
-
-  // client.connect(err => {
-  //   const result = client.db(dbName).collection(collection);
-  //   // perform actions on the collection object
-  //   client.close();
-  // });
 
   return result;
 }
