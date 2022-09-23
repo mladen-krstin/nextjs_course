ad42fbbac471ce63ae8fed361ad0cd5d88e767c3
Mladen Krstin <mladen.krstin@gmail.com>
2022-09-23 18:09:20 +0200

    reverted layout.js

--- nextjs-course-api-routes/components/layout/layout.js
+++ nextjs-course-api-routes/components/layout/layout.js
@@ -1,24 +1,12 @@
-import { Fragment, useContext } from 'react';
+import { Fragment } from 'react';
 
 import MainHeader from './main-header';
-import NotificationContext from '../../store/notification-context';
 
 function Layout(props) {
-  const notificationCtx = useContext(NotificationContext);
-
-  const activeNotification = notificationCtx.notification;
-
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
-      {activeNotification && (
-        <Notification
-          title={activeNotification.title}
-          message={activeNotification.message}
-          status={activeNotification.status}
-        />
-      )}
     </Fragment>
   );
 }
