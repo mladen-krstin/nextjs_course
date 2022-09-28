84665f21cdf2c89913a386e16a795d97f1a2741f
Mladen Krstin <mladen.krstin@gmail.com>
2022-09-28 19:43:04 +0200

    28.09.2022.

--- nextjs-course-code-blog/pages/posts/[slug].js
+++ nextjs-course-code-blog/pages/posts/[slug].js
@@ -1,5 +1,34 @@
+import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
 
-function PostDetailPage() { }
+function PostDetailPage(props) {
+  return <PostContent post={props.post} />
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData
+    },
+    revalidate: 600
+  };
+}
+
+export function getStaticPath() {
+  const postFilenames = getPostsFiles();
+
+  const slugs = postFilenames.map(fileName => fileName.replace(/\.md$/, ''));
+
+  return {
+    paths: slugs.map(slug => ({ params: { slug: slug } })),
+    fallback: true
+  };
+}
 
 export default PostDetailPage;
