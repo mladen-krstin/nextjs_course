321b82ec8d3abaa601af582f31948fba18d3299f
Mladen Krstin <mladen.krstin@gmail.com>
2022-10-01 09:48:41 +0200

    01.10.2022.

---
+++ nextjs-course-code-authentication/lib/db.js
@@ -0,0 +1,9 @@
+import { MongoClient } from 'mongodb';
+
+
+export async function connectToDatabase() {
+  const url = 'mongodb+srv://korisnik:xCtzqePIyRXJ9rn8@cluster0.2uwmvmr.mongodb.net/?retryWrites=true&w=majority';
+  const client = await new MongoClient(url);
+
+  return client;
+};
