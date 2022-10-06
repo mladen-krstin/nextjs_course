a600024d52da408f682a7bb63fb899449e24e760
Mladen Krstin <mladen.krstin@gmail.com>
2022-10-06 18:36:25 +0200

    Final touch - completed authentication module

--- nextjs-course-code-authentication/pages/api/user/change-password.js
+++ nextjs-course-code-authentication/pages/api/user/change-password.js
@@ -1,6 +1,7 @@
 import { getSession } from "next-auth/client";
 
 import { connectToDatabase } from '../../../lib/db';
+import { hashPassword, verifyPassword } from '../../../lib/auth';
 
 
 async function handler(req, res) {
@@ -16,10 +17,38 @@ async function handler(req, res) {
   }
 
   const userEmail = session.user.email;
-  const oldPassword = req.body.oldPasswod;
+  const oldPassword = req.body.oldPassword;
   const newPassword = req.body.newPassword;
 
   const client = await connectToDatabase();
+
+  const usersCollection = client.db('auth-demo').collection('users');
+
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: 'User not found!' });
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (!passwordAreEqual) {
+    res.status(403).json({ message: 'Invalid password!' });
+    return;
+  }
+
+  const hashedPassword = await hashPassword(newPassword);
+
+  const result = await usersCollection.updateOne(
+    { email: userEmail },
+    {
+      $set: { password: hashedPassword }
+    });
+
+  res.status(200).json({ message: 'Password updated!' });
 }
 
 export default handler;
